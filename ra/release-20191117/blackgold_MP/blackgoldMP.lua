f09876c47e8a054059668518b10ac4c9395c8b85
Obnoxieux <34817082+Obnoxieux@users.noreply.github.com>
2019-11-21 18:50:08 +0100

    fall 2019 release, mh60 in blackgold

--- ra/release-20191117/blackgold_MP/blackgoldMP.lua
+++ ra/release-20191117/blackgold_MP/blackgoldMP.lua
@@ -8,7 +8,7 @@ GreeceBaseBuidings = { GreeceKenn, GreeceProc, GreeceWaFa, GreeceDome, GreeceRax
 
 AlliedInfantryTypes = { "e1", "e1", "e1", "e1", "e3", "e3" }
 AlliedArmorTypes = { "1tnk", "1tnk", "1tnk", "2tnk", "2tnk", "2tnk", "2tnk", "2tnk", "2tnk", "2tnk", "jeep", "jeep", "apc", "ctnk", "arty" }
-AlliedAircraftTypes = { "heli" }
+AlliedAircraftTypes = { "mh60", "mh60", "heli" }
 AlliedNavalTypes = { "pt", "pt", "pt", "dd", "dd", "dd", "dd", "dd", "ca" }
 
 EinsteinLabDefenders = { "ctnk", "ctnk", "ctnk", "ctnk" }
@@ -37,7 +37,7 @@ Domf2Kill = { CPos.New(108, 63), CPos.New(109, 63), CPos.New(110, 63), CPos.New(
 Tick = function()
 	if allies.HasNoRequiredUnits() and greece.HasNoRequiredUnits() then
 		ussr1.MarkCompletedObjective(objKillAll)
-		--ussr2.MarkCompletedObjective(objKillAll2)
+		ussr2.MarkCompletedObjective(objKillAll2)
 	end
 end
 
@@ -224,7 +224,7 @@ SendSovietReinforcements = function()
 	Reinforcements.ReinforceWithTransport (ussr1, "lst", SovietReinforcementsSea, { SovietReinfSeaSpawn.Location, SovietReinfSeaUnload.Location})
 	
 	--das hier wird Spieler 2
-	Reinforcements.Reinforce(ussr1, SovietReinforcementsLand, { SovietReinfLandSpawn.Location, SovietReinfLandMove.Location })
+	Reinforcements.Reinforce(ussr2, SovietReinforcementsLand, { SovietReinfLandSpawn.Location, SovietReinfLandMove.Location })
 end
 
 SetupTriggers = function()
@@ -237,13 +237,21 @@ SetupTriggers = function()
 		if not ussr1.IsObjectiveCompleted(objSpyTech) then
 			ussr1.MarkFailedObjective(objSpyTech)
 		end
+		if not ussr2.IsObjectiveCompleted(objSpyTech2) then
+			ussr2.MarkFailedObjective(objSpyTech2)
+		end
 	end)
 
 	Trigger.OnAllKilledOrCaptured(GreeceBaseBuidings, function()
 		ussr1.MarkCompletedObjective(objDestroyBase)
+		ussr2.MarkCompletedObjective(objDestroyBase2)
+		
 		Reinforcements.ReinforceWithTransport (ussr1, "lst", SovietReinforcementsMCV, { SovietReinfSeaSpawn.Location, SovietReinfSeaUnload.Location})
+		Reinforcements.Reinforce(ussr2, SovietReinforcementsMCV, { SovietReinfLandSpawn.Location, SovietReinfLandMove.Location })
+		
 		Media.DisplayMessage("Comrade General, advise to build our base to the West as there is more ore to be found there.")
 		Actor.Create("camera", true, { Owner = ussr1, Location = SovBaseCam.Location })
+		Actor.Create("camera", true, { Owner = ussr2, Location = SovBaseCam.Location })
 		
 		Trigger.AfterDelay(DateTime.Minutes(7), function()
 			ActivateAI()
@@ -254,11 +262,14 @@ SetupTriggers = function()
 
   Trigger.OnKilled(RealDome, function()
 		ussr1.MarkFailedObjective(objFindEinstein)
+		ussr2.MarkFailedObjective(objFindEinstein2)
 	end)
 
 	Trigger.OnCapture(RealDome, function()
 		ussr1.MarkCompletedObjective(objFindEinstein)
+		ussr2.MarkCompletedObjective(objFindEinstein2)
 		Actor.Create("camera", true, { Owner = ussr1, Location = LabCam.Location })
+		Actor.Create("camera", true, { Owner = ussr2, Location = LabCam.Location })
 		
 		if not LabGap1.IsDead then 
 			LabGap1.Kill()
@@ -270,23 +281,23 @@ SetupTriggers = function()
 
 	Trigger.OnKilled(EinsteinLab, function()
 		ussr1.MarkFailedObjective(objInfiltrateLab)
+		ussr2.MarkFailedObjective(objInfiltrateLab2)
 	end)
 
 	Trigger.OnInfiltrated(StolenTechCentre, function()
 		ussr1.MarkCompletedObjective(objSpyTech)
+		ussr2.MarkCompletedObjective(objSpyTech2)
 		
 		Trigger.AfterDelay(DateTime.Seconds(2), function()
 			SendSovietReinforcements()
 			ParadropVolkov()
 		end)
-		
-		--Actor.Create("camera", true, { Owner = ussr1, Location = CamDomf1.Location })
 	end)
 	
 	--kill off the fake domes
 	
   Trigger.OnEnteredFootprint(Domf1Kill, function(actor, trigger)
-		if actor.Owner == ussr1 then
+		if actor.Owner == ussr1 or actor.Owner == ussr2 then
 			Trigger.RemoveFootprintTrigger(trigger)
 			Media.DisplayMessage("This Radar Dome was just a decoy.")
 			if not FakeDome1.IsDead then
@@ -296,7 +307,7 @@ SetupTriggers = function()
 	end)
 
 	Trigger.OnEnteredFootprint(Domf2Kill, function(actor, trigger2)
-		if actor.Owner == ussr1 then
+		if actor.Owner == ussr1 or actor.Owner == ussr2 then
 			Trigger.RemoveFootprintTrigger(trigger2)
 			Media.DisplayMessage("This Radar Dome was just a decoy.")
 			if not FakeDome2.IsDead then
@@ -310,7 +321,9 @@ SetupTriggers = function()
 		if actor.Owner == ussr1 and actor.Type == "gnrl" then
 			Trigger.RemoveProximityTrigger(trigger3)
 			ussr1.MarkCompletedObjective(objInfiltrateLab)
+			ussr2.MarkCompletedObjective(objInfiltrateLab2)
 			ussr1.MarkCompletedObjective(objVolkovSurvival)
+			ussr2.MarkCompletedObjective(objVolkovSurvival2)
 			actor.Destroy()
 		end
 	end)
@@ -320,12 +333,20 @@ end
 InitObjectives = function()
 
 	enemyobj = allies.AddPrimaryObjective("Deny the Soviets.")
+	
   objSpyTech = ussr1.AddPrimaryObjective("Get our spy into our captured tech centre.")
 	objDestroyBase = ussr1.AddPrimaryObjective("Destroy or capture the rest of our former tech base.")
 	objVolkovSurvival = ussr1.AddPrimaryObjective("Volkov must survive.")
 	objFindEinstein = ussr1.AddPrimaryObjective("Find the location of Einstein's lab\nby capturing the Allied radar dome.")
 	objInfiltrateLab = ussr1.AddPrimaryObjective("Get Volkov into Einstein's lab to eliminate him.")
 	objKillAll = ussr1.AddSecondaryObjective("Finish off the Allied forces.")
+	
+  objSpyTech2 = ussr2.AddPrimaryObjective("Help your comrade to get our spy\ninto our captured tech centre.")
+	objDestroyBase2 = ussr2.AddPrimaryObjective("Destroy or capture the rest of our former tech base.")
+	objVolkovSurvival2 = ussr2.AddPrimaryObjective("Your comrade's Volkov must survive.")
+	objFindEinstein2 = ussr2.AddPrimaryObjective("Find the location of Einstein's lab\nby capturing the Allied radar dome.")
+	objInfiltrateLab2 = ussr2.AddPrimaryObjective("Escort your comrade's Volkov into Einstein's lab\nto eliminate him.")
+	objKillAll2 = ussr2.AddSecondaryObjective("Finish off the Allied forces.")
 	
 end
 
@@ -341,7 +362,7 @@ WorldLoaded = function()
 	greece = Player.GetPlayer("Greece")
   einstein = Player.GetPlayer("France")
 	ussr1 = Player.GetPlayer("USSR")
-  --ussr2 = Player.GetPlayer("USSR2")
+  ussr2 = Player.GetPlayer("Ukraine")
 
 	InitObjectives()
   SetupTriggers()
@@ -352,41 +373,41 @@ WorldLoaded = function()
 		Media.DisplayMessage(p.GetObjectiveDescription(id), "New " .. string.lower(p.GetObjectiveType(id)) .. " objective")
   end)
     
-  --[[Trigger.OnObjectiveAdded(ussr2, function(p, id)
+  Trigger.OnObjectiveAdded(ussr2, function(p, id)
     Media.DisplayMessage(p.GetObjectiveDescription(id), "New " .. string.lower(p.GetObjectiveType(id)) .. " objective")
-  end)]]
+  end)
 
   Trigger.OnObjectiveCompleted(ussr1, function(p, id)
     Media.DisplayMessage(p.GetObjectiveDescription(id), "Objective completed")
   end)
     
-  --[[Trigger.OnObjectiveCompleted(ussr2, function(p, id)
+  Trigger.OnObjectiveCompleted(ussr2, function(p, id)
     Media.DisplayMessage(p.GetObjectiveDescription(id), "Objective completed")
-  end)]]
+  end)
 
   Trigger.OnObjectiveFailed(ussr1, function(p, id)
     Media.DisplayMessage(p.GetObjectiveDescription(id), "Objective failed")
   end)
     
-  --[[Trigger.OnObjectiveFailed(ussr2, function(p, id)
+  Trigger.OnObjectiveFailed(ussr2, function(p, id)
     Media.DisplayMessage(p.GetObjectiveDescription(id), "Objective failed")
-  end)]]
+  end)
 
   Trigger.OnPlayerWon(ussr1, function()
     Media.PlaySpeechNotification(ussr1, "MissionAccomplished")
   end)
     
-  --[[Trigger.OnPlayerWon(ussr2, function()
+  Trigger.OnPlayerWon(ussr2, function()
     Media.PlaySpeechNotification(ussr2, "MissionAccomplished")
-  end)]]
+  end)
 
   Trigger.OnPlayerLost(ussr1, function()
     Media.PlaySpeechNotification(ussr1, "MissionFailed")
   end)
     
-  --[[Trigger.OnPlayerLost(ussr2, function()
+  Trigger.OnPlayerLost(ussr2, function()
     Media.PlaySpeechNotification(ussr2, "MissionFailed")
-  end)]]
+  end)
 
   Trigger.AfterDelay(DateTime.Seconds(5), function()
 		StartDogPatrol()
